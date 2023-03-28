dc65b3ffba144e7b2efbf8a20fe828f5ccbedce6
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2023-03-28 16:07:11 +0200

    Implemented simple shutdown protocol

--- client.py
+++ client.py
@@ -5,10 +5,35 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = '192.168.1.201'
 server_port = 31338
 
+client_socket.settimeout(1)
 client_socket.connect((server_address, server_port))
 
-message = 'Hello World'
-client_socket.send(message.encode())
-response = client_socket.recv(1024).decode()
-print(response)
-print("Test passed" if message == response else "Test failed")
+def host_online():
+  client_socket.send("ping".encode())
+  try:
+
+    if client_socket.recv(1024).decode() == "pong":
+      print("Host online")
+      return True
+    else:
+      print("Host responded incorrectly")
+      return False
+  except  TimeoutError:
+      print("host didn't respond")
+      return False
+  except ConnectionRefusedError:
+    print("connection refused")
+    return False
+
+def shutdown_host():
+  if (host_online()):
+    client_socket.send("shutdown".encode())
+    response = client_socket.recv(1024).decode()
+    if response == "ack":
+      print("Host acknowledged shutting down")
+    else:
+      print("Unknown response")
+
+
+shutdown_host()
+
--- host.py
+++ host.py
@@ -1,5 +1,7 @@
 
 import socket
+import os
+import platform
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -10,7 +12,23 @@ server = (server_address, server_port)
 sock.bind(server)
 print("Listening on " + server_address + ":" + str(server_port))
 
+def shutdown():
+  match platform.system():
+    case 'Linux':
+      os.system('systemctl poweroff')
+    case 'Windows':
+      os.system('shutdown /s /t 0')
+
+
 while True:
     payload, client_address = sock.recvfrom(1024)
-    print("Echoing data back to " + str(client_address))
-    sent = sock.sendto(payload, client_address)
+    match payload.decode():
+      case "ping":
+        print("pinged")
+        sock.sendto("pong".encode(), client_address)
+      case "shutdown":
+        print("shutdown")
+        sock.sendto("ack".encode(), client_address)
+        shutdown()
+      case _:
+        print("unknown paylod:" + payload.decode())
