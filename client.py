ac374e8e3530b599703fc17380ac1e82005c8b4b
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2023-03-28 17:35:38 +0200

    autoformatted code

--- client.py
+++ client.py
@@ -8,28 +8,31 @@ server_port = 31338
 client_socket.settimeout(1)
 client_socket.connect((server_address, server_port))
 
+
 def host_online():
-  client_socket.send("ping".encode())
-  try:
-    if client_socket.recv(1024).decode() == "pong":
-      return True #host is online
-    else:
-      return False #host didn't respond correctly
-  except (TimeoutError, ConnectionRefusedError):
-    return False #couldn't connect to host
+    client_socket.send("ping".encode())
+    try:
+        if client_socket.recv(1024).decode() == "pong":
+            return True  # host is online
+        else:
+            return False  # host didn't respond correctly
+    except (TimeoutError, ConnectionRefusedError):
+        return False  # couldn't connect to host
+
 
 def shutdown_host():
-  if (host_online()):
-    print("Host is online")
-    try:
-      client_socket.send("shutdown".encode())
-      response = client_socket.recv(1024).decode()
-      if response == "ack":
-        print("Host acknowledged shutdown request")
-      else:
-        print("Unknown didn't respond correctly for shutdown request")
-    except: (TimeoutError, ConnectionRefusedError):
-      print("Host went offline")
+    if (host_online()):
+        print("Host is online")
+        try:
+            client_socket.send("shutdown".encode())
+            response = client_socket.recv(1024).decode()
+            if response == "ack":
+                print("Host acknowledged shutdown request")
+            else:
+                print("Unknown didn't respond correctly for shutdown request")
+        except:
+            (TimeoutError, ConnectionRefusedError):
+            print("Host went offline")
+
 
 shutdown_host()
-
--- host.py
+++ host.py
@@ -13,23 +13,24 @@ server = (server_address, server_port)
 sock.bind(server)
 print("Listening on " + server_address + ":" + str(server_port))
 
+
 def shutdown():
-  match platform.system():
-    case 'Linux':
-      os.system('systemctl poweroff')
-    case 'Windows':
-      os.system('shutdown /s /t 0')
+    match platform.system():
+        case 'Linux':
+            os.system('systemctl poweroff')
+        case 'Windows':
+            os.system('shutdown /s /t 0')
 
 
 while True:
     payload, client_address = sock.recvfrom(1024)
     match payload.decode():
-      case "ping":
-        print("pinged", file=sys.stderr)
-        sock.sendto("pong".encode(), client_address)
-      case "shutdown":
-        print("shutdown", file=sys.stderr)
-        sock.sendto("ack".encode(), client_address)
-        shutdown()
-      case _:
-        print("unknown payload: " + payload.decode(), file=sys.stderr)
+        case "ping":
+            print("pinged", file=sys.stderr)
+            sock.sendto("pong".encode(), client_address)
+        case "shutdown":
+            print("shutdown", file=sys.stderr)
+            sock.sendto("ack".encode(), client_address)
+            shutdown()
+        case _:
+            print("unknown payload: " + payload.decode(), file=sys.stderr)
